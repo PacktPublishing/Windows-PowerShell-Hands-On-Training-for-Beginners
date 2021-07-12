d72f1bcd8cae4db61b0173d2efb207870d55b548
sanjeetkumar13 <41476387+sanjeetkumar13@users.noreply.github.com>
2021-07-12 16:40:36 +0530

    Resource files added

---
+++ Practice_Files/managing_windows_processes.ps1
@@ -0,0 +1,65 @@
+﻿Start-Process calc
+
+Start-Process calc -PassThru
+
+Start-Process -FilePath "C:\Practice\sample.bat"
+
+Start-Process -FilePath 'C:\Program Files\VideoLAN\VLC\vlc.exe'
+
+
+
+#Get-Process returns a point-in-time snapshot of a system’s running process information.
+
+
+
+Get-Process
+
+
+
+
+Get-Process -Name chrome
+
+
+
+
+Get-Process -Id 10852
+
+
+
+Get-Process -Name Calculator  | Select-Object *
+
+
+Get-Process -Name chrome | Out-GridView
+
+
+
+Get-Process -Name chrome | Select-Object * | Out-GridView
+
+
+Get-Process -Name 'c*' | select -First 1 | select *
+
+
+
+Get-Process -Name 'c*' | Select-Object -Property Name, Company, WorkingSet, Handles, Path | Format-Table
+
+
+
+
+Get-Process -Name 'c*' | Select-Object -Property Name, Company, WorkingSet, Handles, Path | Format-Table -AutoSize
+
+
+Get-Process -Name 'c*' | Select-Object -Property Name, Company, WorkingSet, Handles, Path | Format-List
+
+
+
+Get-Process -Name 'c*' | Select-Object -Property Name, Company, WorkingSet, Handles, Path | Format-Table -AutoSize
+
+
+
+
+Get-Process -Name 'c*' | Where-Object { ($_.Company -like 'Google*')  -and ($_.Handles -gt 500) }  | select *
+
+
+
+
+Get-Process -name chrome | Stop-Process
